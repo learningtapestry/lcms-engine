41a217a1269f0845f58e29938b72bd8acb08a8f7
Abraham Sánchez <abraham@twintags.com>
2018-10-23 18:13:20 +0200

    Initial commit containing base engine files
    - Target Ruby version is 2.3
    - Target Rails version is 4.2
    - Test suite is RSpec
    - Code guidelines and best practices are enforced by Overcommit & Rubocop
    - Test application resides in 'spec/dummy'

---
+++ app/jobs/lcms/engine/application_job.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module Lcms
+  module Engine
+    class ApplicationJob < ActiveJob::Base
+    end
+  end
+end
---
+++ lib/lcms/engine.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'lcms/engine/engine'
+
+module Lcms
+  # Top level module
+  module Engine
+    # Your code goes here...
+  end
+end
