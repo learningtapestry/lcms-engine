e72bf90774fab40d74699a1efdea43072440e1f3
Abraham Sánchez <abraham@twintags.com>
2018-10-30 14:04:34 +0100

    Temporarily disable namespace isolation

--- lib/lcms/engine/engine.rb
+++ lib/lcms/engine/engine.rb
@@ -4,7 +4,8 @@ module Lcms
   module Engine
     # Top level engine class
     class Engine < ::Rails::Engine
-      isolate_namespace Lcms::Engine
+      # TODO: Re-enable namespaces once engine extraction is more stable.
+      # isolate_namespace Lcms::Engine
     end
   end
 end
