5332a976aa9ee868f5d73d615e0c63d403055fe8
Abraham Sánchez <abraham@twintags.com>
2018-11-15 19:57:57 +0100

    Extract service objects

---
+++ app/services/material_build_service.rb
@@ -0,0 +1,102 @@
+# frozen_string_literal: true
+
+require 'lt/lcms/lesson/downloader/gdoc'
+require 'lt/lcms/lesson/downloader/pdf'
+
+class MaterialBuildService
+  PDF_EXT_RE = /\.pdf$/
+
+  def initialize(credentials, opts = {})
+    @credentials = credentials
+    @options = opts
+  end
+
+  def build(url)
+    @url = url
+    pdf? ? build_from_pdf : build_from_gdoc
+  end
+
+  private
+
+  attr_reader :credentials, :material, :downloader, :options, :url
+
+  def build_from_pdf
+    @downloader = ::Lt::Lcms::Lesson::Downloader::PDF.new(credentials, url)
+    create_material
+    title = @downloader.file.name.sub(PDF_EXT_RE, '')
+    identifier = "#{title.downcase}#{ContentPresenter::PDF_EXT}"
+
+    material.update!(
+      material_params.merge(
+        identifier: identifier,
+        metadata: DocTemplate::Objects::MaterialMetadata.build_from_pdf(identifier: identifier, title: title).to_json
+      )
+    )
+
+    material.material_parts.delete_all
+
+    basename = MaterialPresenter.new(material).material_filename
+    pdf_filename = "#{basename}#{ContentPresenter::PDF_EXT}"
+    thumb_filename = "#{basename}#{ContentPresenter::THUMB_EXT}"
+
+    pdf = @downloader.pdf_content
+    thumb_exporter = DocumentExporter::Thumbnail.new(pdf)
+    thumb = thumb_exporter.export
+    material.metadata['orientation'] = thumb_exporter.orientation
+    material.metadata['pdf_url'] = S3Service.upload pdf_filename, pdf
+    material.metadata['thumb_url'] = S3Service.upload thumb_filename, thumb
+    material.save
+    material
+  end
+
+  def build_from_gdoc
+    @downloader = ::Lt::Lcms::Lesson::Downloader::Gdoc.new(@credentials, url, options)
+    create_material
+    content = @downloader.download.content
+    parsed_document = DocTemplate::Template.parse(content, type: :material)
+
+    metadata = parsed_document.metadata_service.options_for(:default)[:metadata]
+    material.update!(
+      material_params.merge(
+        identifier: metadata['identifier'].downcase,
+        metadata: metadata.to_json,
+        original_content: content
+      )
+    )
+
+    material.material_parts.delete_all
+
+    presenter = MaterialPresenter.new(material, parsed_document: parsed_document)
+    DocTemplate.context_types.each do |context_type|
+      material.material_parts.create!(
+        active: true,
+        content: presenter.render_content(context_type),
+        context_type: context_type,
+        part_type: :layout
+      )
+    end
+    material
+  end
+
+  def create_material
+    @material = Material.find_or_initialize_by(file_id: downloader.file_id)
+  end
+
+  def material_params
+    {
+      name: downloader.file.name,
+      last_modified_at: downloader.file.modified_time,
+      last_author_email: downloader.file.last_modifying_user.try(:email_address),
+      last_author_name: downloader.file.last_modifying_user.try(:display_name),
+      reimported_at: Time.current,
+      version: downloader.file.version
+    }
+  end
+
+  def pdf?
+    return options[:source_type].casecmp('pdf').zero? if options[:source_type].present?
+
+    dl = ::Lt::Lcms::Lesson::Downloader::Base.new credentials, url
+    dl.file.name.to_s =~ PDF_EXT_RE
+  end
+end
---
+++ app/services/react_materials_resolver.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+class ReactMaterialsResolver
+  class << self
+    def resolve(html, document)
+      content = Nokogiri::HTML.fragment(html)
+      content.css("[data-react-class='MaterialsContainer']").each do |node|
+        replace_react(node, document)
+      end
+      content.to_html
+    end
+
+    private
+
+    def replace_react(node, document)
+      node.remove && return if (data = node.attr('data-react-props')).blank?
+      raw_props = if data.match?(/^\d+(?:,\s*\d+)*$/)
+                    # comma separated list of numbers, i.e: '123' or '123,432' or '123, 42, 12'
+                    { 'material_ids' => data.split(',').map(&:strip), 'activity' => {} }
+                  else
+                    JSON.parse(data)
+                  end
+      node.remove && return if (raw_props['material_ids']).empty?
+
+      props = PreviewsMaterialSerializer.new(raw_props, document)
+      node.remove && return if props.data&.empty?
+
+      node.replace(component)
+    end
+
+    def component
+      h.react_component('MaterialsContainer', props, prerender: document.content_type != 'none')
+    end
+
+    def h
+      ActionController::Base.helpers
+    end
+  end
+end
