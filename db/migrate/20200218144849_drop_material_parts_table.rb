59d3bf291408374792eeafc274e77e9345802ac1
Alexander Kuznetsov <paranoic.san@gmail.com>
2021-02-12 09:54:36 +0200

    Update DB schema and remove old Rails 5 migrations
    All changes saved inside schema.rb file now.

--- db/migrate/20200218144849_drop_material_parts_table.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class DropMaterialPartsTable < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :material_parts
-  end
-end
--- db/migrate/20200422130652_add_reimported_at_to_documents.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddReimportedAtToDocuments < ActiveRecord::Migration[5.2]
-  def change
-    add_column :documents, :reimported_at, :datetime
-  end
-end
