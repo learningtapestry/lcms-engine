d7ff8e874f49e3f6e0d72bd8f5d10892f57986f6
Alexander Kuznetsov <paranoic.san@gmail.com>
2021-10-25 11:03:50 +0200

    (#194) Issue with filtering by grade
    Fixes #194
    
    Also updates gems.

---
+++ spec/queries/lcms/engine/admin_materials_query_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe Lcms::Engine::AdminMaterialsQuery do
+  describe 'filter by metadata' do
+    let(:query) { { lesson: 1, name_date: true } }
+    let(:scope) { double ActiveRecord::Relation }
+
+    before do
+      allow(Lcms::Engine::Material).to receive(:all).and_return(scope)
+      allow(scope).to receive_message_chain(:reorder, :distinct).and_return(scope)
+    end
+
+    subject { described_class.call query }
+
+    it 'filters by metadata using ILIKE' do
+      expect(scope).to receive(:where_metadata_like).with(:lesson, 1).and_return(scope)
+      expect(scope).to receive(:where_metadata_like).with(:name_date, true).and_return(scope)
+      subject
+    end
+
+    context 'strict metadata fields' do
+      let(:query) { { lesson: 1, grade: 'grade 1', subject: Faker::Lorem.word } }
+
+      it 'filters by metadata using explicit comparison' do
+        expect(scope).to receive(:where_metadata_like).with(:lesson, 1).and_return(scope)
+        expect(scope).to receive(:where_metadata).with(query.slice(:grade)).and_return(scope)
+        expect(scope).to receive(:where_metadata).with(query.slice(:subject)).and_return(scope)
+        subject
+      end
+    end
+  end
+end
