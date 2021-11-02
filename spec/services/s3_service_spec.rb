28c6fac701ece25ef1f5fa466961768f9907b742
Alexander Kuznetsov <paranoic.san@gmail.com>
2021-11-02 10:51:41 +0200

    Allow S3Service to upload with options
    Now one can pass a hash with options for the target S3 object. The list of options can be found in the description of `Aws::S3::Object#put` method.

---
+++ spec/services/s3_service_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe Lcms::Engine::S3Service do
+  describe '.upload' do
+    let(:data) { StringIO.new('data to be uploaded') }
+    let(:full_options) { options.merge(body: data) }
+    let(:key) { 'path/to/file' }
+    let(:object) do
+      params = {
+        put: 'true',
+        public_url: url
+      }
+      double(::Aws::S3::Resource, **params)
+    end
+    let(:options) { { content_type: 'image/png' } }
+    let(:url) { Faker::Internet.url }
+
+    before { allow(described_class).to receive(:create_object).with(key).and_return(object) }
+
+    subject { described_class.upload(key, data, options) }
+
+    it 'uploads data with passed params' do
+      expect(object).to receive(:put).with(full_options)
+      subject
+    end
+
+    it 'returns URL of the uploaded object'
+  end
+end
