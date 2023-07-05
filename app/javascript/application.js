848ed254b8b430ce6e655d69abaaaa9dc4cd846f
Alexander Kuznetsov <paranoic.san@gmail.com>
2023-07-05 08:31:22 +0200

    Fix turbo-rails and bootstrap issue on main layout

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,5 @@
-// import '@hotwired/turbo-rails';
-// import * as bootstrap from 'bootstrap';
+import '@hotwired/turbo-rails';
+import * as bootstrap from 'bootstrap'; // eslint-disable-line
 // import React from 'react';
 // import ReactDOM from 'react-dom';
 
