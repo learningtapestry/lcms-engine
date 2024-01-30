57585d3e7b524ef69fff3992a51a0c5b4205286c
Alexander Kuznetsov <paranoic.san@gmail.com>
2024-01-30 15:52:46 +0200

    Modify handling of flash messages in Lcms::Engine
    Two constants, FLASH_MESSAGE_MAX_CHAR and FLASH_REDIS_PREFIX, are added in Engine module. The flash partial has been updated to handle flash messages differently, checking first if the message originates from Redis or not.
    
    Also, this refactor includes the deletion of engine.rbs under models,
    moving its contents to engine.rbs under lib.

--- lib/lcms/engine.rb
+++ lib/lcms/engine.rb
@@ -2,11 +2,20 @@
 
 module Lcms
   module Engine
-    class << self
+    # Maximum allowed characters for a flash message.
+    # If a message exceeds this limit, it should be handled appropriately.
+    FLASH_MESSAGE_MAX_CHAR = 2048
+
+    # Prefix used when storing flash messages in Redis. This helps in identifying
+    # eys related to flash messages in the Redis store.
+    FLASH_REDIS_PREFIX = 'flash_key:'
+
+    # Returns the table name prefix for the current module or class.
+    #
+    # @return [String] The table name prefix.
+    def self.table_name_prefix
       # TODO: Set the correct prefix after renaming all the tables?
-      def table_name_prefix
-        ''
-      end
+      ''
     end
   end
 end
