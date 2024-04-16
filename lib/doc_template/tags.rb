c52152675b24237fcba64ec907ad14044b2c6389
Alexander Kuznetsov <paranoic.san@gmail.com>
2024-04-16 10:36:32 +0200

    Add simple bold, italic, line break tags support
    This update adds a new function, `substitute_tags_in`, to `DocTemplate::Tags`. This function takes in a string and returns the string with custom tags substituted with their corresponding HTML markup such as [b] for bold, [i] for italic, and [line-break] for line break. Corresponding unit tests are also included.

--- lib/doc_template/tags.rb
+++ lib/doc_template/tags.rb
@@ -3,6 +3,10 @@
 module DocTemplate
   module Tags
     CONFIG_PATH = Rails.root.join('config', 'tags.yml')
+    RE_BOLD = /(\[b\]([^\[]*)\[b:end\])/i
+    RE_ITALIC = /(\[i\]([^\[]*)\[i:end\])/i
+    RE_LINE_BREAK = /\[line-break\]/i
+    TAG_LINE_BREAK = '<br/><br/>'
 
     mattr_accessor :config
 
@@ -10,5 +14,25 @@ module DocTemplate
 
     # By default we remove unknown tags (`DefaultTag`)
     config['default'] ||= { 'remove' => true }
+
+    #
+    # Substitutes the following custom tags with plain HTML markup:
+    #
+    #  # [b]Text[b:end] -> <b>Text</b>
+    #  # [i]Text[i:end] -> <i>Text</i>
+    #  # [line-break] -> <br/><br/>
+    #
+    # @param [String] content Content to substitute vars in
+    # @return [String]
+    def self.substitute_tags_in(content)
+      return '' if content.blank?
+
+      # @type var content: String
+      content
+        .gsub("\n", '<br/>')
+        .gsub(RE_LINE_BREAK, TAG_LINE_BREAK)
+        .gsub(RE_ITALIC, '<i>\2</i>')
+        .gsub(RE_BOLD, '<b>\2</b>')
+    end
   end
 end
--- spec/lib/doc_template/tags_spec.rb
+++ spec/lib/doc_template/tags_spec.rb
@@ -8,4 +8,24 @@ describe DocTemplate::Tags do
       expect(described_class.config).to be_a(Hash)
     end
   end
+
+  describe '.substitute_tags_in' do
+    context 'when content is blank' do
+      it 'returns an empty string' do
+        expect(described_class.substitute_tags_in('')).to eq('')
+        expect(described_class.substitute_tags_in(nil)).to eq('')
+      end
+    end
+
+    context 'when content is not blank' do
+      let(:content) { "This is [b]bold[b:end]. \n This is [i]italic[i:end]" }
+      let(:tag_content) { 'This is <b>bold</b>. <br/> This is <i>italic</i>' }
+
+      subject { described_class.substitute_tags_in(content) }
+
+      it 'returns content with substitutions' do
+        expect(subject).to eq(tag_content)
+      end
+    end
+  end
 end
