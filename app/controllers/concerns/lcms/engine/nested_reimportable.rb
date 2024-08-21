5eb518074cf079a02e86d273492cd7f3d1de4903
Oksana Melnikova <oksana.melnikova@gmail.com>
2024-08-21 10:57:16 +0300

    fixed collecting errors for nested jobs

--- app/controllers/concerns/lcms/engine/nested_reimportable.rb
+++ app/controllers/concerns/lcms/engine/nested_reimportable.rb
@@ -32,8 +32,8 @@ module Lcms
         return jid_res unless result_nested.any? { _1['ok'] == false }
 
         { ok: false, errors: jid_res&.[]('errors') || [] }.tap do |failed_result|
-          result_nested.each do |e|
-            failed_result[:errors] << "<a href=\"#{e['link']}\">Source</a>: #{e['errors'].join(', ')}"
+          result_nested.select { _1['ok'] == false }.each do |e|
+            failed_result[:errors] << "<a href=\"#{e['link']}\">Source</a>: #{(e['errors'] || []).join(', ')}"
           end
         end
       end
