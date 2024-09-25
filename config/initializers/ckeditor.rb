e3e973bd08050e5c57d1a26c8f957bca61227790
Alexander Kuznetsov <paranoic.san@gmail.com>
2024-09-25 14:33:16 +0700

    Update CKEditor version
    Upgraded CKEditor from version 4.20.0 to 4.22.1 and changed gem dependency to ensure minimum version 5.1.3.
    
    4.22 is the last Open Source version available. For now, we should hide
    the fake warning about security. It seems the warning is shown to move
    users to v5 and buy the license.

--- config/initializers/ckeditor.rb
+++ config/initializers/ckeditor.rb
@@ -2,5 +2,5 @@
 
 Ckeditor.setup do |config|
   # //cdn.ckeditor.com/<version.number>/<distribution>/ckeditor.js
-  config.cdn_url = '//cdn.ckeditor.com/4.20.0/standard/ckeditor.js'
+  config.cdn_url = '//cdn.ckeditor.com/4.22.1/standard/ckeditor.js'
 end
