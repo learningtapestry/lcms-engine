9a0c44d9afbea5b922e2c44a5dcf8a7a04449486
Oksana Melnikova <oksana.melnikova@gmail.com>
2025-04-24 22:04:22 +0400

    [hotfix] fixed tests

--- spec/services/s3_service_spec.rb
+++ spec/services/s3_service_spec.rb
@@ -5,14 +5,17 @@ require 'rails_helper'
 describe Lcms::Engine::S3Service do
   describe '.upload' do
     let(:data) { StringIO.new('data to be uploaded') }
-    let(:full_options) { options.merge(body: data) }
+    let(:full_options) do
+      options.merge(body: data, cache_control: 'public, max-age=0, must-revalidate',
+                    metadata_directive: 'REPLACE')
+    end
     let(:key) { 'path/to/file' }
     let(:object) do
       params = {
         put: 'true',
         public_url: url
       }
-      double(::Aws::S3::Resource, **params)
+      double(Aws::S3::Resource, **params)
     end
     let(:options) { { content_type: 'image/png' } }
     let(:url) { Faker::Internet.url }
